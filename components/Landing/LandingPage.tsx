590fb63a2cd777ef4e696264db41efbfb0d70a35
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-29 00:27:28 +0600

    added slide animation section

--- components/Landing/LandingPage.tsx
+++ components/Landing/LandingPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "@/components/Header/Header";
 import HeroSection from "@/components/Hero/Hero";
+import TapeSection from "@/components/Tape/Tape";
 
 const LandingPage = () => {
   return (
     <div className="relative">
       <Header />
       <HeroSection />
+      <TapeSection />
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         sans: "var(--font-sans)",
         serif: "var(--font-serif)",
       },
+      animation: {
+        "move-left": "move-left 1s linear infinite",
+      },
+      keyframes: {
+        "move-left": {
+          "0%": {
+            transform: "translateX(0%)",
+          },
+          "100%": {
+            transform: "translateX(-50%)",
+          },
+        },
+      },
     },
     screens: {
       sm: "375px",
