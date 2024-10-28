86257900ba04070856fa25f0a3abb535061818f0
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-28 22:13:52 +0600

    update header section

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,24 +1,25 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-center items-center relative top-3">
       <nav className="flex gap-1 p-0.5 rounded-full bg-white/10 backdrop-blur border border-white/15">
-        <a href="" className="nav-item">
+        <Link href={"/"} className="nav-item">
           Home
-        </a>
-        <a href="" className="nav-item">
+        </Link>
+        <Link href={"/projects"} className="nav-item">
           Projects
-        </a>
-        <a href="" className="nav-item">
+        </Link>
+        <Link href={"/about"} className="nav-item">
           About
-        </a>
-        <a
-          href=""
+        </Link>
+        <Link
+          href={"/contact"}
           className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
         >
           Contact
-        </a>
+        </Link>
       </nav>
     </div>
   );
